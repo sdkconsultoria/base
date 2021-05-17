541d1c1ec842e52ed34c88452351051aef7739dd
Camilo Rodriguez <hellsythex@gmail.com>
2021-05-16 21:11:45 -0500

    Added translate

--- src/Http/Controllers/Admin/KeyController.php
+++ src/Http/Controllers/Admin/KeyController.php
@@ -3,9 +3,12 @@
 namespace Sdkconsultoria\Base\Http\Controllers\Admin;
 
 use Sdkconsultoria\Base\Http\Controllers\ResourceController;
+use Sdkconsultoria\Base\Traits\TranslateController;
 
 class KeyController extends ResourceController
 {
+    use TranslateController;
+
     protected $model = \Sdkconsultoria\Base\Models\Common\Key\Key::class;
     protected $translate = \Sdkconsultoria\Base\Models\Common\Key\KeyTranslate::class;
     protected $view = 'base::back.key.';
--- src/Http/Controllers/Admin/KeyGroupController.php
+++ src/Http/Controllers/Admin/KeyGroupController.php
@@ -3,9 +3,12 @@
 namespace Sdkconsultoria\Base\Http\Controllers\Admin;
 
 use Sdkconsultoria\Base\Http\Controllers\ResourceController;
+use Sdkconsultoria\Base\Traits\TranslateController;
 
 class KeyGroupController extends ResourceController
 {
+    use TranslateController;
+
     protected $model = \Sdkconsultoria\Base\Models\Common\Key\KeyGroup::class;
     protected $translate = \Sdkconsultoria\Base\Models\Common\Key\KeyGroupTranslate::class;
     protected $view = 'base::back.key-group.';

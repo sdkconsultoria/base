c789ba4e34a8f621cddf0c28f33a8f6d862299a7
Kamilo Rodriguez <hellsythex@gmail.com>
2021-10-01 16:44:50 -0500

    Added seeder

---
+++ stubs/seeders/DatabaseSeeder.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        \Database\Factories\UserFactory::new()->count(10)->create();
+    }
+}

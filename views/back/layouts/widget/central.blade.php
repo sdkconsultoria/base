03dd90bc6a62b449360fe8562631eb4426d17886
Camilo Rodriguez <hellsythex@gmail.com>
2022-03-27 12:00:10 -0600

    Added background animation

--- views/back/layouts/widget/central.blade.php
+++ views/back/layouts/widget/central.blade.php
@@ -4,7 +4,7 @@
 <div class="p-3 min-h-full flex flex-colum flex-wrap content-center justify-center">
     <div class="lg:w-2/6 flex flex-col bg-white rounded-3xl p-10">
         <div class="flex flex-row justify-center">
-            <?= Base::img('images/logo_sdk.png', ['width' => '30%']) ?>
+            <img src="{{ asset('img/logo.svg') }}" width="40%" class="h-24" />
         </div>
         @yield('central')
     </div>

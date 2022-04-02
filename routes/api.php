be0c1a68b763eefe62d74dcb88e12efbc5971e92
Camilo Rodriguez <hellsythex@gmail.com>
2022-04-01 18:46:39 -0600

    remove api routes

--- routes/api.php
+++ routes/api.php
@@ -6,6 +6,4 @@ use Illuminate\Support\Facades\Route;
 Route::namespace('\Sdkconsultoria\Base\Http\Controllers\Admin')
 ->middleware('auth:sanctum')
 ->prefix('api/v1')->group(function () {
-    Route::SdkApiResource('blog', 'Blog\BlogController');
-    Route::SdkApiResource('blog-post', 'Blog\BlogPostController');
 });

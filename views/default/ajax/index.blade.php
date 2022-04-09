874b3f76dc0af2db5982323cb6fc247249558bce
Camilo Rodriguez <hellsythex@gmail.com>
2022-04-08 19:13:58 -0500

    removed useless files

--- views/default/ajax/index.blade.php
+++
@@ -1,9 +0,0 @@
-@section('partial_form')
-    <?= Base::breadcrumb([$model->getLabel('plural')]) ?>
-
-    <div class="mb-2">
-        <a type="button" href="{{$model->getRoute('create')}}" class="btn btn-primary"> {!! $model->getTranslate('create') !!} </a>
-    </div>
-
-    @yield('model')
-@endsection

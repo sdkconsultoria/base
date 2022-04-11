630d947f33c8f8e2d73dad4e2e500737f59ec70a
Camilo Rodriguez <hellsythex@gmail.com>
2022-04-10 19:40:34 -0500

    Fixed

--- views/back/default/show.blade.php
+++ views/back/default/show.blade.php
@@ -5,10 +5,10 @@
 @section('content')
     <?= Base::breadcrumb([ $model->getRoute('index') => $model->getTranslation('plural'), $model->getTranslation('showed')]) ?>
 
-    {{-- <div class="mb-2 flex flex-row">
-        <a type="button" href="{{$model->getRoute('edit', $model->getKeyId())}}" class="btn btn-primary"> {!!  $model->getTranslate('edit') !!} </a>
+    <div class="mb-2 flex flex-row">
+        <a type="button" href="{{$model->getRoute('update', $model->getKeyId())}}" class="btn btn-primary"> {!!  $model->getTranslation('edit') !!} </a>
 
-        <form
+        {{-- <form
         data-title="{{ $model::getTranslate('delete_question') }}"
         data-confirm="{{ $model::getTranslate('delete') }}"
         data-cancel="{{ __('base::app.common.cancel') }}"
@@ -16,9 +16,9 @@
             @csrf
             <input type="hidden" name="_method" value="DELETE">
             <button class="btn btn-danger ml-2" type="submit"> {{$model::getTranslate('delete')}}</button>
-        </form>
+        </form> --}}
 
-    </div> --}}
+    </div>
 
     @yield('model')
 @endsection

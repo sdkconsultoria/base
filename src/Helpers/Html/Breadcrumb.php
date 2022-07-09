1f7bd8396daa4323004cb0358eedf756d31151fe
Camilo Rodriguez <hellsythex@gmail.com>
2022-07-09 16:46:29 -0500

    fixed with pint

--- src/Helpers/Html/Breadcrumb.php
+++ src/Helpers/Html/Breadcrumb.php
@@ -28,7 +28,7 @@ class Breadcrumb extends BaseHtml implements iHtml
             $html .= '<li>'.Base::icon('folder', ['class' => 'h-5 mr-1']).'<a href="'.$key.'">'.$value.'</a></li>';
         }
 
-        $html .= '<li>' .Base::icon('folder-open', ['class' => 'h-5 mr-1']).$current.'</li>';
+        $html .= '<li>'.Base::icon('folder-open', ['class' => 'h-5 mr-1']).$current.'</li>';
 
         return $html;
     }
@@ -43,7 +43,7 @@ class Breadcrumb extends BaseHtml implements iHtml
         $html = '
             <div class="text-sm breadcrumbs mb-3">
                 <ul>
-                    <li><a href="'.route('dashboard').'"> ' . Base::icon('folder', ['class' => 'h-5 mr-1']) . 'Dashboard</a></li>
+                    <li><a href="'.route('dashboard').'"> '.Base::icon('folder', ['class' => 'h-5 mr-1']).'Dashboard</a></li>
                     '.$this->items().'
                 </ul>
             </div>
--- src/Http/Controllers/Admin/Auth/UserController.php
+++ src/Http/Controllers/Admin/Auth/UserController.php
@@ -10,6 +10,5 @@ class UserController extends ResourceController
 
     public function myAccount()
     {
-
     }
 }

67fd1dcdca1c0e8f9a98cd23dbb4ec22d3f93bef
Camilo Rodriguez <hellsythex@gmail.com>
2022-07-09 17:49:48 -0500

    Fixed Central

--- views/back/layouts/widget/central.blade.php
+++ views/back/layouts/widget/central.blade.php
@@ -1,8 +1,8 @@
 @extends('base::back.layouts.empty')
 
 @section('content')
-<div class="p-3 min-h-full flex flex-colum flex-wrap content-center justify-center">
-    <div class="lg:w-2/6 flex flex-col bg-white rounded-3xl p-10">
+<div class="p-3 min-h-full flex flex-colum flex-wrap content-center justify-center drop-shadow-2xl">
+    <div class="lg:w-2/6 flex flex-col bg-neutral-content rounded-3xl p-10">
         <div class="flex flex-row justify-center">
             <img src="{{ asset('img/logo.svg') }}" width="40%" class="h-24" />
         </div>

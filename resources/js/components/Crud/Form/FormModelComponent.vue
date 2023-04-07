e603e20e19da23ea13bcbab0b2271b063066f007
Camilo Rodríguez <camilo@sdkconsultoria.com>
2023-04-06 21:43:39 -0600

    Update bg

--- resources/js/components/Crud/Form/FormModelComponent.vue
+++ resources/js/components/Crud/Form/FormModelComponent.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="p-4 bg-white mb-5 shadow rounded-lg">
+  <div class="p-4 bg-base-200 mb-5 shadow rounded-lg">
     <form
       id="form-create"
       :action="`${routes.api}`"
--- stubs/views/_form.blade.php
+++ stubs/views/_form.blade.php
@@ -1,2 +1,2 @@
-<div class="p-4 bg-white mb-5 shadow rounded-lg">
+<div class="p-4 bg-base-200 mb-5 shadow rounded-lg">
 </div>

8530ee56cd1798d78f0bf1580e448d00703f8cfc
Camilo Rodríguez <camilo@sdkconsultoria.com>
2023-04-23 17:23:56 -0600

    Added support

--- resources/js/components/Crud/GridView/GridViewComponent.vue
+++ resources/js/components/Crud/GridView/GridViewComponent.vue
@@ -1,32 +1,26 @@
 <template>
-  <div>
-    <SearchComponent :create_route="create_route" :translations="translations" />
-    <table class="table w-full mt-3 mb-3">
-        <thead>
-        <tr>
-            <OrderComponent
-            v-for="field in fields"
-            :key="field"
-            :field="field"
-            :current_order="current_order"
-            :label="translations[field]"
-            />
-            <th width="100px"></th>
-        </tr>
-        </thead>
-        <tbody>
-        <tr v-for="model in data.data" :key="model.id">
-            <td
-            v-for="field in fields"
-            :key="field"
-            >
-            {{ getValue(model, field) }}
-            </td>
-            <td><ActionColumn :template_actions="template_actions" :model_id="model.id"/></td>
-        </tr>
-        </tbody>
-    </table>
-    <PaginationComponent :data="data" />
+    <div>
+        <SearchComponent :create_route="create_route" :translations="translations" />
+        <table class="table w-full mt-3 mb-3">
+            <thead>
+                <tr>
+                    <OrderComponent v-for="field in fields" :key="field" :field="field" :current_order="current_order"
+                        :label="translations[field]" />
+                    <th width="100px"></th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr v-for="model in data.data" :key="model.id">
+                    <td v-for="field in fields" :key="field">
+                        {{ getValue(model, field) }}
+                    </td>
+                    <td>
+                        <ActionColumn :template_actions="template_actions" :model_id="model.id" />
+                    </td>
+                </tr>
+            </tbody>
+        </table>
+        <PaginationComponent :data="data" />
     </div>
 </template>
 
@@ -38,78 +32,92 @@ import SearchComponent from "./SearchComponent.vue";
 import ActionColumn from "./ActionColumn.vue";
 
 export default {
-  name: "GridView",
-  props: {
-    fields: Array,
-    translations: JSON,
-    routes: JSON,
-    filters: JSON,
-    create_route: String,
-    template_actions: JSON,
-  },
-  components: {
-    PaginationComponent,
-    OrderComponent,
-    SearchComponent,
-    ActionColumn,
-  },
+    name: "GridView",
+    props: {
+        fields: Array,
+        translations: JSON,
+        routes: JSON,
+        filters: JSON,
+        create_route: String,
+        template_actions: JSON,
+    },
+    components: {
+        PaginationComponent,
+        OrderComponent,
+        SearchComponent,
+        ActionColumn,
+    },
 
-  methods: {
-    getValue(model, field) {
-        let array = field.split('.');
+    methods: {
+        getValue(model, field) {
+            let array = field.split('.');
 
-        if (array.length > 1) {
-            return model[array[0]][array[1]];
-        } else {
-            return model[field];
+            if (array.length > 1) {
+                let value  = model[array[0]] ?? '-';
+                for (let index = 1; index < array.length; index++) {
+                    if (value == '-') {
+                        return value;
+                    }
+                    value = value[array[index]] ?? '-';
+                }
+                return value;
+
+            } else {
+                return model[field] ?? '-';
+            }
+        },
+        getSubpropierty(array)
+        {
+
         }
-    }
-  },
-  setup(props) {
-    const data = ref({});
-    const loading = ref(true);
-    const error = ref(null);
-    const current_order = ref({
-      field: "",
-      order: "",
-    });
-    function addQueryToUrl(url) {
-      return url + window.location.search;
-    }
+    },
+    setup(props) {
+        const data = ref({});
+        const loading = ref(true);
+        const error = ref(null);
+        const current_order = ref({
+            field: "",
+            order: "",
+        });
+        function addQueryToUrl(url) {
+            return url + window.location.search;
+        }
 
-    function fetchData(url) {
+        function fetchData(url) {
 
-      const cookieValue = document.cookie
-      .split('; ')
-      .find(row => row.startsWith('XSRF-TOKEN='))
-      .split('=')[1];
+            const cookieValue = document.cookie
+                .split('; ')
+                .find(row => row.startsWith('XSRF-TOKEN='))
+                .split('=')[1];
 
-      loading.value = true;
-      return fetch(url, {"headers":{
-        "Accept": "application/json",
-        'X-XSRF-TOKEN': cookieValue
-        }})
-        .then((response) => response.json())
-        .then((response) => {
-          data.value = response;
-        })
-        .catch((error) => {
-          // handle the error
-        })
-        .then(() => (loading.value = false));
-    }
+            loading.value = true;
+            return fetch(url, {
+                "headers": {
+                    "Accept": "application/json",
+                    'X-XSRF-TOKEN': cookieValue
+                }
+            })
+                .then((response) => response.json())
+                .then((response) => {
+                    data.value = response;
+                })
+                .catch((error) => {
+                    // handle the error
+                })
+                .then(() => (loading.value = false));
+        }
 
-    onMounted(() => {
-      fetchData(addQueryToUrl(props.routes.api));
-    });
+        onMounted(() => {
+            fetchData(addQueryToUrl(props.routes.api));
+        });
 
-    return {
-      data,
-      loading,
-      error,
-      current_order,
-      fetchData,
-    };
-  },
+        return {
+            data,
+            loading,
+            error,
+            current_order,
+            fetchData,
+        };
+    },
 };
 </script>
